c0a32a145e647fbfa2877410fffbb451de28658a
TAROS Web Services <dev@taros.ch>
2026-03-02 14:30:02 +0200

    fix: resolve circular dependency in AttendeesModule with forwardRef

--- Server/src/attendees/attendees.module.ts
+++ Server/src/attendees/attendees.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AttendeesService } from './attendees.service';
 import { AttendeesController } from './attendees.controller';
 import { OutgoingWebhooksModule } from '../outgoing-webhooks/outgoing-webhooks.module';
 import { EventsModule } from '../events/events.module';
 
 @Module({
-  imports: [OutgoingWebhooksModule, EventsModule],
+  imports: [OutgoingWebhooksModule, forwardRef(() => EventsModule)],
   providers: [AttendeesService],
   controllers: [AttendeesController],
   exports: [AttendeesService],
